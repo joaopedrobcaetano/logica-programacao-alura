12aab1db8ea49a57a26a37e408b3adc7667d1a37
joaopedrobcaetano <joaopedrobcaetano@gmail.com>
2024-01-17 13:31:59 -0300

    teste chute

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,7 @@
+alert("Bem-vindo ao jogo do número secreto!");
+let numeroSecreto = 29;
+let chute = prompt("Escolha um número entre 1 e 30");
+
+if (chute == numeroSecreto) {
+    console.log("Parabéns! Você acertou o número secreto (29)")
+}
