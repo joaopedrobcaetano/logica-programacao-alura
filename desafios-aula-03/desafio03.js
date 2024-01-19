caf594bc744b7d8c3ee15b2426e0b713dda3d24b
joaopedrobcaetano <joaopedrobcaetano@gmail.com>
2024-01-19 14:05:10 -0300

    fazendo desafios aula 03

---
+++ desafios-aula-03/desafio03.js
@@ -0,0 +1,5 @@
+let contador = prompt('Digite um número positivo para fazer a contagem regressiva: ')
+while (contador >= 0) {
+    alert(contador)
+    contador--
+}
---
+++ desafios-aula-03/desafio04.js
@@ -0,0 +1,6 @@
+let contador = 0
+let contarAte = prompt('Digite um número positivo para fazer a contagem progressiva:')
+while (contador <= contarAte) {
+    alert(contador)
+    contador++
+}
