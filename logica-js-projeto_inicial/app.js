5669fd7bdb9cb1582cd9038c97add0e1ebfdd839
joaopedrobcaetano <joaopedrobcaetano@gmail.com>
2024-01-19 09:19:38 -0300

    incluindo while e número de tentativas

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,12 +1,22 @@
 alert("Bem-vindo ao jogo do número secreto!");
 let numeroSecreto = 16;
-console.log("Número secreto: ", numeroSecreto)
-let chute = prompt("Escolha um número entre 1 e 30");
-console.log("Valor do chute: ", chute)
-console.log("Resultado da comparação: ", chute == numeroSecreto)
+console.log("Número secreto: ", numeroSecreto);
+let chute;
+let tentativas = 1;
+console.log("Valor do chute: ", chute);
+console.log("Resultado da comparação: ", chute == numeroSecreto);
 
-if (chute == numeroSecreto) {
-    alert(`Parabéns! Você acertou o número secreto (${numeroSecreto})`)
-} else {
-    alert("Que pena, você errou :(")
+while (chute != numeroSecreto) {
+    chute = prompt("Escolha um número entre 1 e 30");
+    if (chute == numeroSecreto) {
+        alert(`Parabéns! Você acertou o número secreto (${numeroSecreto}), com ${tentativas} tentativas.`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}.`);
+        } else {
+            alert(`O número secreto é maior que ${chute}.`);
+        }
+        tentativas++
+    }
 }
+
