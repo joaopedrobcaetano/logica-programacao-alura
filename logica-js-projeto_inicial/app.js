8c90941b03d8001cb2f507f77614bec4b199102f
joaopedrobcaetano <joaopedrobcaetano@gmail.com>
2024-01-21 23:27:01 -0300

    fazendo aula 04

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,5 +1,6 @@
 alert("Bem-vindo ao jogo do número secreto!");
-let numeroSecreto = 16;
+let numeroMaximo = 100;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log("Número secreto: ", numeroSecreto);
 let chute;
 let tentativas = 1;
@@ -7,16 +8,18 @@ console.log("Valor do chute: ", chute);
 console.log("Resultado da comparação: ", chute == numeroSecreto);
 
 while (chute != numeroSecreto) {
-    chute = prompt("Escolha um número entre 1 e 30");
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
     if (chute == numeroSecreto) {
-        alert(`Parabéns! Você acertou o número secreto (${numeroSecreto}), com ${tentativas} tentativas.`);
+        break;
     } else {
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}.`);
         } else {
             alert(`O número secreto é maior que ${chute}.`);
         }
-        tentativas++
+        tentativas++;
     }
 }
 
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Parabéns! Você acertou o número secreto (${numeroSecreto}), com ${tentativas} ${palavraTentativa}.`);
