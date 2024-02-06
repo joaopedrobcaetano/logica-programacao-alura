799aba862bcbe161e3d36c768670005c1f54ea4e
joaopedrobcaetano <joaopedrobcaetano@gmail.com>
2024-02-06 17:34:44 -0300

    fazendo aula 04

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -1,3 +1,5 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -21,6 +23,7 @@ function verificarChute() {
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
         document.getElementById('reiniciar').removeAttribute('disabled');
+        document.getElementById('chutarNumero').setAttribute('disabled', true);
     } else if (chute > numeroSecreto) {
         exibirTextoNaTela('p', 'O número secreto é menor');
     } else {
@@ -31,7 +34,19 @@ function verificarChute() {
 }
 
 function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 10 + 1);
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length();
+
+    if (quantidadeDeElementosNaLista == numeroLimite) {
+        listaDeNumerosSorteados = [];
+    }
+
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumerosSorteados.push();
+        return numeroEscolhido;
+    }
 }
 
 function limparCampo() {
@@ -45,4 +60,5 @@ function reiniciarJogo() {
     tentativas = 1;
     exibirMensagemInicial();
     document.getElementById('reiniciar').setAttribute('disabled', true);
+    document.getElementById('chutarNumero').removeAttribute('disabled');
 }
